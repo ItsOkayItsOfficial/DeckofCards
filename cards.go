0c31ab3d90dcf326aff0d022cc5743a933f06d55
Alex P <alexp@itsokayitsofficial.io>
2019-08-07 20:13:48 -0700

    Initialized

---
+++ cards.go
@@ -0,0 +1,20 @@
+package main
+
+// Randomize array of Cards
+func shuffle() {
+
+}
+
+// Returns boolean for value in array of Cards
+func checkDeck() {
+
+}
+
+// Return and remove one value of array of Cards
+func dealOne() {
+
+}
+
+func main() {
+
+}
