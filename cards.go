b6d3aab6a309eca219a4c00385927800744e0975
Alex P <alexp@itsokayitsofficial.io>
2019-08-08 12:25:28 -0700

    Compiled and run. Stable

--- cards.go
+++ cards.go
@@ -16,9 +16,12 @@ type Deck []Card
 
 // Build deck using defined Card into Deck
 func buildDeck() (deck Deck) {
+
+	// String arrays defining Card values
 	ranks := []string{"Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
 	suits := []string{"Heart", "Diamond", "Club", "Spade"}
 
+	// Populate our Deck array of Card values
 	for i := 0; i < len(ranks); i++ {
 		for n := 0; n < len(suits); n++ {
 			card := Card{
@@ -28,31 +31,45 @@ func buildDeck() (deck Deck) {
 			deck = append(deck, card)
 		}
 	}
+
 	return
 }
 
 // Randomize array of Cards
 func shuffleDeck(deck Deck) Deck {
+
 	// "pseudo"-randomize order of elements
 	rand.Shuffle(len(deck), func(i, j int) {
 		deck[i], deck[j] = deck[j], deck[i]
 	})
+
 	return deck
 }
 
 // Return and remove last value of array of Cards
 func dealOne(deck Deck) Deck {
 
+	// Print Card and then remove
 	if len(deck) > 0 {
 		fmt.Println(deck[len(deck)])
 		deck = deck[:len(deck)-1]
+	} else {
+		// No Card left in Deck array
+		fmt.Println("No cards left to deal. Deck is empty!")
 	}
-
-	fmt.Println("No cards left to deal. Deck is empty!")
 
 	return deck
 }
 
+// Ideally we'll make this on recall
 func main() {
 
+	// Get a deck of cards
+	deck := buildDeck()
+
+	// Shuffle deck of cards
+	shuffleDeck(deck)
+
+	// Deal one card
+	dealOne(deck)
 }
